954a7ddb15a8b2bf3095dfa4102ba157fe448d94
Nuatu Tseggai & Carolyn Huynh <student@epicodus.com>
2014-08-01 14:46:42 -0700

    properly render UI choices/navigation

--- to_do_interface.rb
+++ to_do_interface.rb
@@ -6,7 +6,7 @@ require './lib/task'
 def main_menu
   loop  do
     puts "Press '1' to create a list."
-    puts "Press '2' to list all list(s)."
+    puts "Press '2' to modify list(s)."
     puts "Press 'x' to exit."
 
     main_choice = gets.chomp
@@ -26,7 +26,11 @@ end
 
 def create_list
   puts "List name?"
+  puts "Press '0' to exit."
   name = gets.chomp
+  if name == 0
+    main_menu
+  end
   new_list = List.new(name)
   puts "New list added.\n"
   new_list.save
@@ -36,43 +40,53 @@ def create_list
 end
 
 def list_lists
-  puts "Press index of list to modify or '0' to exit"
+  List.all.each_with_index do |list, index|
+  puts "#{index+1}. #{list.name}"
+  end
+  puts "\n\nPress index of list to modify or '0' to main menu.\n\n"
   @current_list = gets.chomp.to_i
 
   if @current_list == 0
     puts "Goodbye"
     main_menu
   else
-    puts "Press '1' to delete this list."
-    puts "Press '2' to add task."
-    modify_choice = gets.chomp.to_i
+    puts "\n\nPress '1' to delete this list."
+    puts "Press '2' to add a task to this list."
+    modify_choice = gets.chomp
 
-    if modify_choice == 1
+    if modify_choice == "1"
       List.all.delete_at(@current_list-1)
-    elsif modify_choice == 2
+      puts "List deleted\n\n"
+      list_lists
+    elsif modify_choice == "2"
       create_task
     end
   end
 end
 
 def create_task
-  puts "Enter name of new task."
-  puts "Press '0' to go to main menu."
-  puts "Press '1' to go back to lists."
+  List.all[@current_list-1].tasks.each_with_index do |task, index|
+  puts "#{index+1}. #{task.name}"
+  end
+  puts "\n\nPress '1' to enter task name."
+  puts "Press '2' to go to main menu."
+  puts "Press '3' to go back to lists.\n\n"
+
   name = gets.chomp
-  if name == 0
+
+  if name == "2"
     puts "Goodbye"
     main_menu
-  elsif name == 1
+  elsif name == "3"
     list_lists
-  end
+  elsif name == "1"
+    puts "Task name?"
+    new_task = Task.new(gets.chomp)
+    List.all[@current_list-1].tasks.push(new_task)
+    puts "\nTask added.\n\n"
 
-  new_task = Task.new(name)
-  puts "Task added.\n\n"
-  # List.all[current_list-1].tasks.push(new_task)
-  List.all[@current_list-1].tasks.push(new_task)
-  puts List.all[@current_list-1].tasks
-  create_task
+    create_task
+  end
 end
 
 main_menu
